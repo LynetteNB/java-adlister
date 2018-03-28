e2b331ef243b2008c862b4543225620f397332df
Lynette <lyna8907@gmail.com>
2018-03-28 10:40:03 -0500

    completed drill 2 with color pages

---
+++ src/main/java/colorPickerServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "colorPickerServlet", urlPatterns = "/pickcolor")
+public class colorPickerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String color = request.getParameter("color");
+        request.setAttribute("color", color);
+        request.getRequestDispatcher("/WEB-INF/showColor.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pickColor.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/pickColor.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lynette
+  Date: 3/28/18
+  Time: 10:34 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Favorite Color</title>
+</head>
+<body>
+<form action="/pickcolor" method="POST">
+    <label>What's your favorite color?
+        <input type="text" name="color">
+    </label>
+</form>
+</body>
+</html>
