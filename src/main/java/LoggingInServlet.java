2ddbe7524191f1d7dcae6c2ecda956df2aac592e
Lynette <lyna8907@gmail.com>
2018-03-28 13:35:32 -0500

    made logginInServlet and WEB-INF directory

---
+++ src/main/java/LoggingInServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login")
+public class LoggingInServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        request.setAttribute("username", username);
+        request.setAttribute("password", password);
+        if (username != null && password != null && username.equals("admin") && password.equals("password")) {
+            response.sendRedirect("/profile");
+        }
+    }
+}
